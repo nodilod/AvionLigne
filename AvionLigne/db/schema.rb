9bcd325fc5b66dd28ecbb424f6f2d210ac02f891
gcharasson <103490686+Pacefiregab@users.noreply.github.com>
2023-04-11 15:19:31 +0200

    :sparkles: add flights associations to airports

--- AvionLigne/db/schema.rb
+++ AvionLigne/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_11_121257) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_11_124510) do
   create_table "airports", force: :cascade do |t|
     t.string "international_code"
     t.string "name"
@@ -26,6 +26,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_11_121257) do
     t.datetime "arrival_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "arrival_airport_id"
+    t.integer "departure_airport_id"
+    t.index ["arrival_airport_id"], name: "index_flights_on_arrival_airport_id"
+    t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
   end
 
+  add_foreign_key "flights", "airports", column: "arrival_airport_id"
+  add_foreign_key "flights", "airports", column: "departure_airport_id"
 end
