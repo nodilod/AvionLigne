864de2269b7dece4994bf12cd550f6375f6a25b3
Victor <victor.josso33@gmail.com>
2023-06-06 15:27:27 +0200

    seeder almost working, flights are broken

--- AvionLigne3/db/seeds.rb
+++ AvionLigne3/db/seeds.rb
@@ -3,5 +3,28 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+#
+require 'csv'
+
+CSV.foreach(Rails.root.join('lib/seed_csv/flights.csv'), headers: true) do |row|
+  Flight.create( {
+                   id: row['number'].to_i,
+                   departure_airport_id: row['departure_airport'].to_i,
+                   arrival_airport_id: row['arrival_airport'].to_i,
+                   business_class_seat: row['business_class_seats'].to_i,
+                   economic_class_seat: row['economy_class_seats'].to_i,
+                   departure_date: row['departure_date'].to_datetime,
+                   arrival_date: row['departure_date'].to_datetime + row['duration'].to_i.minutes,
+                 } )
+end
+
+CSV.foreach(Rails.root.join('lib/seed_csv/airports.csv'), headers: true) do |row|
+  Airport.create( {
+                   id: row['airport_id'].to_i,
+                   international_code: row['international_code'],
+                   name: row['name'],
+                   city: row['city'],
+                 } )
+end
