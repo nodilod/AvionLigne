a64de50d54f382ac6ebcad21ab64875a2113a048
Grégoire <nodilod@gmail.com>
2023-06-06 17:22:25 +0200

    :ambulance: fix user

--- AvionLigne3/db/schema.rb
+++ AvionLigne3/db/schema.rb
@@ -66,7 +66,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_133714) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "role"
+    t.string "role", default: "user"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
